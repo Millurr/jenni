39865ceaba26d6a97cca8328016b0fbfd10c4e7d
Millurr <42452986+Millurr@users.noreply.github.com>
2020-12-18 14:51:41 -0600

    .

--- mern-auth-back/routes/frontend/routes.js
+++ mern-auth-back/routes/frontend/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', async(req, res) => {
     const indexPath = path.join('client', 'build', 'index.html');
-
+    console.log(IndexPath);
     res.sendFile(indexPath);
 });
 
